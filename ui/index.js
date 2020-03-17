d1ec7bbc6ba32a179fb815c4f23d882f08b6468c
eug-vs <eug-vs@keemail.me>
2020-03-17 20:36:36 +0300

    feat: paginate and filter addPosts() results

--- ui/index.js
+++ ui/index.js
@@ -5,14 +5,30 @@
     try {
       if (posts) return true;
     } catch (e) {
-      console.error("Posts data is not loaded!")
+      console.error('Posts data is not loaded!');
     }
     return false;
   }
 
-  logic.getPosts = () =>  {
+  const filterPosts = (posts, filterConfig) => {
+    if (!filterConfig) return posts;
+    let results = posts;
+    const { date, authorId } = filterConfig;
+    if (authorId) results = results.filter(post => post.author.id === authorId);
+    if (date) {
+      if (date.before || date.after) {
+        if (date.before) results = results.filter(post => post.createdAt >= date.before);
+        if (date.after) results = results.filter(post => post.createdAt <= date.after);
+      } else results = results.filter(post.createdAt === date);
+    }
+    return results;
+  }
+
+  logic.getPosts = (skip = 0, top = 10, filterConfig) => {
     if (!postsLoaded()) return [];
-    return posts;
+    const paginated = posts.slice(skip, skip + top);
+    const sorted = paginated.sort((a, b) => a.createdAt < b.createdAt);
+    return filterPosts(sorted, filterConfig);
   }
 
   window.logic = logic;
