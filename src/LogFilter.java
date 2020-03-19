ba7057978318adca032835c73bdda27f1082be0d
Eug-VS <eug-vs@keemail.me>
2020-03-19 16:48:20 +0300

    fix: do not split lines while logging them

--- src/LogFilter.java
+++ src/LogFilter.java
@@ -11,14 +11,18 @@ import javax.servlet.http.HttpServletResponse;
 public class LogFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    System.out.print(String.format("%s: %s", httpRequest.getMethod(), httpRequest.getRequestURL()));
-
     long startTime =  System.currentTimeMillis();
     chain.doFilter(request, response);
     long endTime = System.currentTimeMillis();
 
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
-    System.out.println(String.format(" - %s (%d ms)", httpResponse.getStatus(), endTime - startTime));
+    System.out.println(String.format(
+        "%s: %s - %s (%d ms)",
+        httpRequest.getMethod(),
+        httpRequest.getRequestURL(),
+        httpResponse.getStatus(),
+        endTime - startTime
+    ));
   }
 }
