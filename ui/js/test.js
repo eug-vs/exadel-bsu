7c14fa37b838293d7e8802554f1111a5b9071300
Eug-VS <eug-vs@keemail.me>
2020-03-24 18:34:16 +0300

    style: temporarily disable eslint for old files

--- ui/js/test.js
+++ ui/js/test.js
@@ -1,4 +1,5 @@
-;(() => {
+/* eslint-disable */
+(() => {
   const test = {};
 
   const logicLoaded = () => {
@@ -8,9 +9,9 @@
       console.error('Logic module is not loaded!');
     }
     return false;
-  }
+  };
 
-  const logFunctionCall = (func, params, name="") => {
+  const logFunctionCall = (func, params, name = '') => {
     let result;
     if (params.length) result = func(...params);
     else result = func(params);
@@ -21,9 +22,9 @@
       console.log(`${name}(${params})`);
     }
     console.log(result);
-  }
+  };
 
-  const referenceDate = new Date("2020-02-15T23:00:00");
+  const referenceDate = new Date('2020-02-15T23:00:00');
 
   const testData = {
     getPosts: [
@@ -31,10 +32,10 @@
       [0, 5],
       [5, 3],
       [0, 10, { authorId: 2 }],
-      [0, 10, { date: { before: referenceDate }}],
-      [0, 10, { date: { after: referenceDate }}],
+      [0, 10, { date: { before: referenceDate } }],
+      [0, 10, { date: { after: referenceDate } }],
       [0, 10, { date: { after: referenceDate }, authorId: 1 }],
-      [0, 10, { hashTag: 'cool' }]
+      [0, 10, { hashTag: 'cool' }],
     ],
 
     getPost: [1, 2, 15],
@@ -42,19 +43,19 @@
     addPost: [
       {
         id: 100,
-        content: "This post was created by addPost() function.",
+        content: 'This post was created by addPost() function.',
         author: {
           id: 2,
-          name: "John",
-          surname: "Doe"
+          name: 'John',
+          surname: 'Doe',
         },
         createdAt: referenceDate,
-      }
+      },
     ],
 
     editPost: [
-      [1, { content: "This post was edited with editPost() function." }],
-      [7, { content: "This post was edited with editPost() function.", createdAt: new Date() }]
+      [1, { content: 'This post was edited with editPost() function.' }],
+      [7, { content: 'This post was edited with editPost() function.', createdAt: new Date() }],
     ],
 
     removePost: [1, 2, 10],
@@ -62,34 +63,33 @@
     validatePost: [
       {
         id: 300,
-        content: "This is the valid post example.",
+        content: 'This is the valid post example.',
         author: {
           id: 2,
-          name: "John",
-          surname: "Doe"
+          name: 'John',
+          surname: 'Doe',
         },
         createdAt: referenceDate,
       },
       {
         id: 300,
-        content: "This post is invalid since it has no createdAt field.",
+        content: 'This post is invalid since it has no createdAt field.',
         author: {
           id: 2,
-          name: "John",
-          surname: "Doe"
+          name: 'John',
+          surname: 'Doe',
         },
-      }
-    ]
+      },
+    ],
   };
 
   // Generate test functions and assign them to test module object
   Object.keys(testData).forEach(functionName => {
     test[functionName] = () => {
-      if(!logicLoaded()) return false;
+      if (!logicLoaded()) return false;
       testData[functionName].forEach(params => logFunctionCall(logic[functionName], params, functionName));
     };
   });
 
   window.test = test;
 })();
-
