db464a7cc0858f90f174accfe0186c20451c1aaa
eug-vs <eug-vs@keemail.me>
2020-04-08 16:34:28 +0300

    fix: change Check endpoint method

--- src/CheckServlet.java
+++ src/CheckServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CheckServlet extends HttpServlet {
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
     response.getOutputStream().println("{ \"success\": true }");
   }
