8f9226631169ddf4e043199a265103cb6198d886
eug-vs <eug-vs@keemail.me>
2020-04-13 00:02:20 +0300

    feat: add create and delete methods in Manager

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -6,12 +6,32 @@ public class Manager<T extends Entity> {
 
   public Manager() {
     objects = new ArrayList<T>();
-    nextId = 0;
+    nextId = 1;
   }
 
-  public void register(T object) {
+  private void register(T object) {
     object.setId(nextId);
     nextId += 1;
+  }
+
+  public void create(T object) {
+    register(object);
+    objects.add(object);
+  }
+
+  public T get(int id) {
+    for (T object : objects) {
+      if (object.getId() == id) {
+        return object;
+      }
+    }
+    return null;
+  }
+
+  public void delete(int id) {
+    for (int i = 0; i < objects.size(); i++) {
+      if (objects.get(i).getId() == id) objects.remove(i);
+    }
   }
 }
 
