5fad7c83f4255e78c471ba2533657261f1e3f02d
eug-vs <eug-vs@keemail.me>
2020-04-13 10:15:35 +0300

    feat: implement PUT method in UserServlet

--- src/main/java/UserServlet.java
+++ src/main/java/UserServlet.java
@@ -5,6 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class UserServlet extends GlobalServlet {
   @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String name = request.getParameter("name");
+    String surname = request.getParameter("surname");
+    User user = new User(name, surname);
+    users.register(user);
+    response.setStatus(HttpServletResponse.SC_CREATED);
+  }
+
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     int id = Integer.parseInt(request.getParameter("id"));
     User user = users.get(id);
@@ -15,12 +24,19 @@ public class UserServlet extends GlobalServlet {
   }
 
   @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    int id = Integer.parseInt(request.getParameter("id"));
+    User user = users.get(id);
+
     String name = request.getParameter("name");
+    if (name != null) {
+      user.setName(name);
+    }
+
     String surname = request.getParameter("surname");
-    User user = new User(name, surname);
-    users.register(user);
-    response.setStatus(HttpServletResponse.SC_CREATED);
+    if (surname != null) {
+      user.setSurname(surname);
+    }
   }
 
   @Override
