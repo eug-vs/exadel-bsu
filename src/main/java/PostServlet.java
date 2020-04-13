a40ec14d44c07e399689540b6a3330ec6c6641fd
eug-vs <eug-vs@keemail.me>
2020-04-13 10:19:43 +0300

    feat: complete CRUD operations in PostServlet

--- src/main/java/PostServlet.java
+++ src/main/java/PostServlet.java
@@ -5,6 +5,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class PostServlet extends GlobalServlet {
   @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String content = request.getParameter("content");
+    int authorId = Integer.parseInt(request.getParameter("id"));
+    Post post = new Post(authorId, content);
+    posts.register(post);
+    response.setStatus(HttpServletResponse.SC_CREATED);
+  }
+
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     int id = Integer.parseInt(request.getParameter("id"));
     Post post = posts.get(id);
@@ -12,6 +21,22 @@ public class PostServlet extends GlobalServlet {
       response.setContentType("application/json");
       response.getOutputStream().println(post.toString());
     } else response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+  }
+
+  @Override
+  protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    int id = Integer.parseInt(request.getParameter("id"));
+    Post post = posts.get(id);
+
+    int authorId = Integer.parseInt(request.getParameter("id"));
+    if (authorId > 0) {
+      post.setAuthorId(authorId);
+    }
+
+    String content = request.getParameter("content");
+    if (content != null) {
+      post.setContent(content);
+    }
   }
 
   @Override
