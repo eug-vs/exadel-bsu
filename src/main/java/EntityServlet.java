ed83652d74ff113fce74182e41fca8609f489ad7
eug-vs <eug-vs@keemail.me>
2020-04-14 21:24:06 +0300

    refactor: transfer core func. to EntityServlet

--- src/main/java/EntityServlet.java
+++ src/main/java/EntityServlet.java
@@ -1,19 +1,70 @@
+import java.util.ArrayList;
+import java.io.IOException;
+import java.lang.NumberFormatException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public abstract class EntityServlet<T extends Entity> extends GlobalServlet {
+
   public Service<T> entities;
 
   protected abstract T createInstance(HttpServletRequest request);
 
   protected abstract void modifyInstance(HttpServletRequest request, T instance);
 
+  private void returnInstance(HttpServletResponse response, T instance) {
+    try {
+      JSONObject json = new JSONObject(instance);
+      response.setContentType("application/json");
+      response.getOutputStream().println(json.toString());
+    } catch (IOException e) {
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private void returnMultiple(HttpServletResponse response, ArrayList<T> instances) {
+    try {
+      JSONArray json = new JSONArray();
+      for (T instance : instances) {
+        JSONObject instanceJson = new JSONObject(instance);
+        json.put(instanceJson);
+      }
+      response.setContentType("application/json");
+      response.getOutputStream().println(json.toString());
+    } catch (IOException e) {
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  private int parseId(HttpServletRequest request) {
+    String id = null;
+    String endpoint = request.getPathInfo();
+
+    if (endpoint == null) {
+      id = request.getParameter("id");
+    } else if (endpoint.matches("/\\d+")) {
+      id = endpoint.substring(1);
+    }
+
+    if (id == null) return 0;
+
+    try {
+      return Integer.parseInt(id);
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
+
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     int id = parseId(request);
 
     if (id == 0) {
-      returnMultiple(response, users.getObjects());
+      returnMultiple(response, entities.getObjects());
     } else if (id == -1) {
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
     } else {
--- src/main/java/GlobalServlet.java
+++ src/main/java/GlobalServlet.java
@@ -1,13 +1,4 @@
-import java.util.ArrayList;
-import java.io.IOException;
-import java.lang.NumberFormatException;
-
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletRequest;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public abstract class GlobalServlet extends HttpServlet {
   public static Service<User> users;
@@ -30,46 +21,4 @@ public abstract class GlobalServlet extends HttpServlet {
     posts.register(new Post(2, "Hello, world!"));
   }
 
-  protected <T extends Entity> void returnInstance(HttpServletResponse response, T instance) {
-    try {
-      JSONObject json = new JSONObject(instance);
-      response.setContentType("application/json");
-      response.getOutputStream().println(json.toString());
-    } catch (IOException e) {
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    }
-  }
-
-  protected <T extends Entity> void returnMultiple(HttpServletResponse response, ArrayList<T> instances) {
-    try {
-      JSONArray json = new JSONArray();
-      for (T instance : instances) {
-        JSONObject instanceJson = new JSONObject(instance);
-        json.put(instanceJson);
-      }
-      response.setContentType("application/json");
-      response.getOutputStream().println(json.toString());
-    } catch (IOException e) {
-      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    }
-  }
-
-  protected int parseId(HttpServletRequest request) {
-    String id = null;
-    String endpoint = request.getPathInfo();
-
-    if (endpoint == null) {
-      id = request.getParameter("id");
-    } else if (endpoint.matches("/\\d+")) {
-      id = endpoint.substring(1);
-    }
-
-    if (id == null) return 0;
-
-    try {
-      return Integer.parseInt(id);
-    } catch (NumberFormatException e) {
-      return -1;
-    }
-  }
 }
